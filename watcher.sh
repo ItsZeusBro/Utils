1371f8e5846a965377f993ead6fc97151eebebff
Zeus <zeusofolympus1000@gmail.com>
2022-12-18 19:42:28 -0500

    watcher.sh works with C reference and watches file with two arguments targetDir and targetFile

--- watcher.sh
+++ watcher.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #https://stackoverflow.com/questions/66857291/how-to-execute-a-shell-script-when-a-file-changes
-echo "$(clear; gcc ./Reference/Reference.c; ./Reference/a.out)"
-echo "$(gcc $1/$2;$1/a.out)"
-LTIME=$(stat -f %Sm ./Test.c)
+echo "$(clear; cd ./Reference; gcc ./Reference/Reference.c; ./Reference/a.out; cd ..)";
+echo "$(gcc $1/$2; $1/a.out);";
+LTIME=$(stat -f %Sm $1/$2);
 while true
 do
   ATIME=$(stat -f %Sa $1/$2)
   if [[ "$ATIME" != "$LTIME" ]]; then
-        echo "$(gcc $1/$2; $1/a.out"
+        echo "$(./Reference/a.out)";
+        echo "$(gcc $1/$2;$1/a.out)";
         LTIME=$ATIME
   fi
   sleep 1
