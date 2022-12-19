8cefddddbffd74c197fb7a0b969ed6a938e86c9b
Zeus <zeusofolympus1000@gmail.com>
2022-12-18 22:06:20 -0500

    watch for [conditionals] in bash they need a space [ condition ]

--- Rand/Rand.c
+++ Rand/Rand.c
@@ -4,14 +4,15 @@
 
 struct String * genStr(int size){
     
-    string * str = (string *) malloc(sizeof(string));
-    // str.str = (char *) malloc(size*sizeof(char));
+    struct String * str = (struct String *) malloc(sizeof(string));
+    str.str = (char *) malloc(size*sizeof(char));
+    
     // srand((unsigned) time(&t));
     // for(int i = 0; i<size; i++){
     //     char charCode = (rand() % 127)+1;
     //     str.str[i]=charCode;
     // }
-    return str&
+    return str;
 }
 
 // int main(){
--- String/watcher.sh
+++ String/watcher.sh
@@ -2,22 +2,23 @@
 #https://stackoverflow.com/questions/66857291/how-to-execute-a-shell-script-when-a-file-changes
 echo "$(clear; cd ../Reference; gcc ../Reference/Reference.c; ../Reference/a.out; cd ../String;)";
 echo "$(make; ./stringTest;)";
-TIME1=$(stat -f %Sm $1;);
-TIME2=$(stat -f %Sm $2;);
-TIME3=$(stat -f %Sm $3;);
 
+TIMEA=$(stat -f %Sm $1;);
+TIMEB=$(stat -f %Sm $2;);
+TIMEC=$(stat -f %Sm $3;);
 while true
 do
-  ATIME1=$(stat -f %Sa $1;);
-  ATIME2=$(stat -f %Sa $2;);
-  ATIME3=$(stat -f %Sm $3;);
 
-  if [[ ("$ATIME1" != "$TIME1") && ("$ATIME2" != "$TIME2") && ("$ATIME3" != "$TIME3")]]; then
+  ATIMEA=$(stat -f %Sa $1;);
+  ATIMEB=$(stat -f %Sa $2;);
+  ATIMEC=$(stat -f %Sa $3;);
+
+  if [[ "$ATIMEA" != "$TIMEA" ]] || [[ "$ATIMEB" != "$TIMEB" ]] || [[ "$ATIMEC" != "$TIMEC" ]]; then 
         echo "$(../Reference/a.out;)";
         echo "$(make; ./stringTest;)";
-        TIME1=$ATIME1;
-        TIME2=$ATIME2;
-        TIME3=$ATIME3;
+        TIMEA=$ATIMEA;
+        TIMEB=$ATIMEB;
+        TIMEC=$ATIMEC;
   fi
   sleep 1
 done
