ab20243d9bf29d454c0fbd348d386906ae283e4c
Zeus <zeusofolympus1000@gmail.com>
2022-12-19 21:37:29 -0500

    some random generator interfaces

--- Rand/Rand.c
+++ Rand/Rand.c
@@ -3,54 +3,27 @@
 #include <stdlib.h>
 #include <time.h> 
 
-//A char in c is an 8 byte (256 ascii possibilities) memory buffer
-//if we wanted to support other types of unicode charachters we need our own
-//structs and files that support the memory management
-struct String * genStr(int min, int max){
-    
+struct Number * genNumber(struct randNumber * _randNumber){
+    int number = (min + rand() % (max+1 - min));      //https://stackoverflow.com/questions/29381843/generate-random-number-in-range-min-max
+}
+
+struct LinkList * genNumber(struct randLinkList * _randLinkList){
+
+}
+
+struct Struct * genStruct(struct randStruct * _randStruct){
+
+}
+
+struct String * genStr(struct randStr * _randStr){
     struct String * str = (struct String *) malloc(sizeof(string));
-    struct String * str2 = (struct String *) malloc(sizeof(string));
-
-    //Casting to (char *) allows us to do pointer arithmetic like next++ or whatever
-    //if we did not cast malloc's return pointer, we would only have byte pointer
-    str->str = (char *) malloc(size*sizeof(char));
-    str2->str = malloc(size*sizeof(char));
-    printf("%d", sizeof(char));
-    printf("str %p\n", str->str);
-    str->str++;
-    printf("str %p\n", str->str);
-    printf("str2 %p\n", str2->str);
-    str2->str+=(2*(sizeof(char)));
-    printf("str2 %p\n", str2->str);
-
-    // str->size=size;
-    // str->prev=&str->str;
-    // str->next=&str->str;
-    // str->next++;
-
-    // srand((unsigned) time(NULL));
-    // for(int i = 0; i<size; i++){
-    //     char charCode = (rand() % 127)+1;
-    //     str->str
-    // }
-
-    return str;
-}
-
-int genInt(struct randInt * randIntStruct){
-    //https://stackoverflow.com/questions/29381843/generate-random-number-in-range-min-max
-    return (min + rand() % (max+1 - min));
-}
-
-void * genStruct(struct randStruct * randStructStruct){
-    //takes a struct prototype and randomly fills it according to the types in the _struct
-}
-
-
-void * genArray(void * _struct){
 
 }
 
-float genFloat(float min, float max){
-    
+struct Array * genArray(struct randArr * _randArr){
+
+}
+
+struct Float * genFloat(struct randFloat * _randFloat){
+
 }
--- Types/LinkList/LinkList.h
+++ Types/LinkList/LinkList.h
@@ -5,3 +5,4 @@ struct Node{
     void * Data;
     struct Node * next;
 }
+
