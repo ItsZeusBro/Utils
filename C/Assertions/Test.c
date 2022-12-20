2b32a4a660463ff5a71c7aa2093eac1d4e4cfa37
Zeus <zeusofolympus1000@gmail.com>
2022-12-20 14:28:25 -0500

    separating C from Javascript

---
+++ C/Assertions/Test.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <float.h>
+#include <assert.h>
+
+int hexEqual(){
+
+}
+
+int b64Equal(){
+
+}
+
+int binEqual(){
+
+}
+
+int byt8Equal(){
+
+}
+
+int byt16Equal(){
+
+}
+
+int byt32Equal(){
+
+}
+
+int byt64Equal(){
+
+}
+
+int numberEqual(){
+
+}
+
+int floatEqual(){
+
+}
+
+int stringsEqual(){
+
+}
+
+int arrayEqual(){
+
+}
+
+
+int isHex(){
+
+}
+
+int isb64(){
+
+}
+
+int isbin(){
+
+}
+
+int isByt8(){
+
+}
+
+int isByt16(){
+
+}
+
+int isByt32(){
+
+}
+
+int isByt64(){
+
+}
+
+int isAscii(){
+
+}
+
+int isUnicode(){
+
+}
+
+int isFloat(){
+
+}
+
+int isNumber(){
+
+}
+
+int isString(){
+    
+}
+
+int isArray(){
+
+}
+
+int main(){
+    hexEqual();
+    b64Equal();
+    binEqual();
+    byt8Equal();
+    byt16Equal();
+    byt32Equal();
+    byt64Equal();
+    numberEqual();
+    floatEqual();
+    stringsEqual();
+    arrayEqual();
+    isHex();
+    isb64();
+    isbin();
+    isByt8();
+    isByt16();
+    isByt32();
+    isByt64();
+    isAscii();
+    isUnicode();
+    isFloat();
+    isNumber();
+    isString();
+    isArray();
+}
