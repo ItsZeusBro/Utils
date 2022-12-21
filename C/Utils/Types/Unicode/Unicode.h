f7d2dd94f572aaec02c09210af1a7f4c0337bcc4
Zeus <zeusofolympus1000@gmail.com>
2022-12-21 01:14:57 -0500

    had to fix these

--- C/Utils/Types/Unicode/Unicode.h
+++ C/Utils/Types/Unicode/Unicode.h
@@ -1,18 +1,18 @@
 #ifndef UNICODE_FILE
 #define UNICODE_FILE
 
-struct Unicode{
-    int _char;
-    int nBytes;
-};
+// struct Unicode{
+//     int _char;
+//     int nBytes;
+// };
 
-struct Unicode * _mallocUnicode(int nBytes);
+// struct Unicode * _mallocUnicode(int nBytes);
 
-struct Unicode * _callocUnicode(int nBytes);
+// struct Unicode * _callocUnicode(int nBytes);
 
-struct Unicode * unicode(int unicode);
+// struct Unicode * unicode(int unicode);
 
-void printUnicode(int charCode);
+// void printUnicode(int charCode);
 
 
 
