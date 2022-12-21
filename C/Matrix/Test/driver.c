140756a64ab75e5a64d4e2c51433430b1226e92b
Zeus <zeusofolympus1000@gmail.com>
2022-12-21 18:15:23 -0500

    matrixDevRun

---
+++ C/Matrix/Test/driver.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Matrix Test Driver\n");
+
+    return 0;
+}
---
+++ C/Matrix/Test/driver.h
@@ -0,0 +1,5 @@
+#ifndef MATRIX_DRIVER_FILE
+#define MATRIX_DRIVER_FILE
+#include "./Test.h"
+
+#endif
