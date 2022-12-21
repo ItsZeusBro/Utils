70dba50b45d3c02372885b955f1005e4dc53e0fa
Zeus <zeusofolympus1000@gmail.com>
2022-12-21 17:48:07 -0500

    we got it to work

--- C/Automata/Test/driver.c
+++ C/Automata/Test/driver.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    printf("Automata Test Driver");
-    for(int i = 0; i<10000000; i++){
-        for(int j = 0; j<10000000; j++){
+    printf("Automata Test Driver\n\n");
 
-        }
-    }
     return 0;
 }
