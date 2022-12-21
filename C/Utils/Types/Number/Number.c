dab7fc3492b7205b494a88279416a6f445eb61da
Zeus <zeusofolympus1000@gmail.com>
2022-12-21 14:45:38 -0500

    its coming together

--- C/Utils/Types/Number/Number.c
+++ C/Utils/Types/Number/Number.c
@@ -6,215 +6,215 @@
 #include <float.h>
 #include <assert.h>
 
-struct Number * number(char * number, char * fromType, char * toType, char * fromEndian, char * toEndian){
+// struct Number * number(char * number, char * fromType, char * toType, char * fromEndian, char * toEndian){
 
-}
+// }
 
-struct DEC * DEC(char * dec){
+// struct DEC * DEC(char * dec){
 
-}
-struct BIN * BIN(char * bin, char * fromEndian, char * toEndian){
+// }
+// struct BIN * BIN(char * bin, char * fromEndian, char * toEndian){
 
-}
-struct HEX * HEX(char * hex, char * fromEndian, char * toEndian){
+// }
+// struct HEX * HEX(char * hex, char * fromEndian, char * toEndian){
 
-}
-struct B64 * B64(char * b64, char * fromEndian, char * toEndian){
+// }
+// struct B64 * B64(char * b64, char * fromEndian, char * toEndian){
 
-}
-struct BYT8 * BYT8(char * byt8, char * fromEndian, char * toEndian){
+// }
+// struct BYT8 * BYT8(char * byt8, char * fromEndian, char * toEndian){
 
-}
-struct BYT16 * BYT16(char * byt16, char * fromEndian, char * toEndian){
+// }
+// struct BYT16 * BYT16(char * byt16, char * fromEndian, char * toEndian){
 
-}
-struct BYT32 * BYT32(char * byt32, char * fromEndian, char * toEndian){
+// }
+// struct BYT32 * BYT32(char * byt32, char * fromEndian, char * toEndian){
 
-}
-struct BYT64 * BYT64(char * byt64, char * fromEndian, char * toEndian){
+// }
+// struct BYT64 * BYT64(char * byt64, char * fromEndian, char * toEndian){
 
-}
+// }
 
-//DECIMAL CONVERSIONS
-struct HEX * dec2hex(struct DEC * dec, char * toEndian){
+// //DECIMAL CONVERSIONS
+// struct HEX * dec2hex(struct DEC * dec, char * toEndian){
 
-}
-struct B64 * dec2b64(struct DEC * dec, char * toEndian){
+// }
+// struct B64 * dec2b64(struct DEC * dec, char * toEndian){
 
-}
-struct BIN * dec2bin(struct DEC * dec, char * toEndian){
+// }
+// struct BIN * dec2bin(struct DEC * dec, char * toEndian){
 
-}
-struct BYT8 * dec2byt8(struct DEC * dec, char * toEndian){
+// }
+// struct BYT8 * dec2byt8(struct DEC * dec, char * toEndian){
 
-}
-struct BYT16 * dec2byt16(struct DEC * dec, char * toEndian){
+// }
+// struct BYT16 * dec2byt16(struct DEC * dec, char * toEndian){
 
-}
-char * dec2byt32(struct DEC * dec, char * toEndian){
+// }
+// char * dec2byt32(struct DEC * dec, char * toEndian){
 
-}
-char * dec2byt64(struct DEC * dec, char * toEndian){
+// }
+// char * dec2byt64(struct DEC * dec, char * toEndian){
 
-}
+// }
 
-//HEXIDECIMAL CONVERSIONS
-struct DEC * hex2dec(struct HEX * hex, char * fromEndian){
+// //HEXIDECIMAL CONVERSIONS
+// struct DEC * hex2dec(struct HEX * hex, char * fromEndian){
 
-}
-struct B64 * hex2b64(struct HEX * hex, char * fromEndian){
+// }
+// struct B64 * hex2b64(struct HEX * hex, char * fromEndian){
 
-}
-struct BIN * hex2bin(struct HEX * hex, char * fromEndian){
+// }
+// struct BIN * hex2bin(struct HEX * hex, char * fromEndian){
 
-}
-struct BYT8 * hex2byt8(struct HEX * hex, char * fromEndian){
+// }
+// struct BYT8 * hex2byt8(struct HEX * hex, char * fromEndian){
 
-}
-struct BYT16 * hex2byt16(struct HEX * hex, char * fromEndian){
+// }
+// struct BYT16 * hex2byt16(struct HEX * hex, char * fromEndian){
 
-}
-char * hex2byt32(struct HEX * hex, char * fromEndian){
+// }
+// char * hex2byt32(struct HEX * hex, char * fromEndian){
 
-}
-char * hex2byt64(struct HEX * hex, char * fromEndian){
+// }
+// char * hex2byt64(struct HEX * hex, char * fromEndian){
 
-}
+// }
 
-//B64 CONVERSIONS
-struct DEC * b642dec(struct B64 * b64, char * fromEndian){
+// //B64 CONVERSIONS
+// struct DEC * b642dec(struct B64 * b64, char * fromEndian){
 
-}
-struct HEX * b642hex(struct B64 * b64, char * fromEndian, char * toEndian){
+// }
+// struct HEX * b642hex(struct B64 * b64, char * fromEndian, char * toEndian){
 
-}
-struct BIN * b642bin(struct B64 * b64, char * fromEndian, char * toEndian){
+// }
+// struct BIN * b642bin(struct B64 * b64, char * fromEndian, char * toEndian){
 
-}
-struct BYT8 * b642byt8(struct B64 * b64, char * fromEndian, char * toEndian){
+// }
+// struct BYT8 * b642byt8(struct B64 * b64, char * fromEndian, char * toEndian){
 
-}
-struct BYT16 * b642byt16(struct B64 * b64, char * fromEndian, char * toEndian){
+// }
+// struct BYT16 * b642byt16(struct B64 * b64, char * fromEndian, char * toEndian){
 
-}
-char * b642byt32(struct B64 * b64, char * fromEndian, char * toEndian){
+// }
+// char * b642byt32(struct B64 * b64, char * fromEndian, char * toEndian){
 
-}
-char * b642byt64(struct B64 * b64, char * fromEndian, char * toEndian){
+// }
+// char * b642byt64(struct B64 * b64, char * fromEndian, char * toEndian){
 
-}
+// }
 
-//BINARY CONVERSIONS
-struct DEC * bin2dec(struct BIN * bin, char * fromEndian){
+// //BINARY CONVERSIONS
+// struct DEC * bin2dec(struct BIN * bin, char * fromEndian){
 
-}
-struct HEX * bin2hex(struct BIN * bin, char * fromEndian, char * toEndian){
+// }
+// struct HEX * bin2hex(struct BIN * bin, char * fromEndian, char * toEndian){
 
-}
-struct B64 * bin2b64(struct BIN * bin, char * fromEndian, char * toEndian){
+// }
+// struct B64 * bin2b64(struct BIN * bin, char * fromEndian, char * toEndian){
 
-}
-struct BYT8 * bin2byt8(struct BIN * bin, char * fromEndian, char * toEndians){
+// }
+// struct BYT8 * bin2byt8(struct BIN * bin, char * fromEndian, char * toEndians){
 
-}
-struct BYT16 * bin2byt16(struct BIN * bin, char * fromEndian, char * toEndian){
+// }
+// struct BYT16 * bin2byt16(struct BIN * bin, char * fromEndian, char * toEndian){
 
-}
-struct BYT32 * bin2byt32(struct BIN * bin, char * fromEndian, char * toEndians){
+// }
+// struct BYT32 * bin2byt32(struct BIN * bin, char * fromEndian, char * toEndians){
 
-}
-struct BYT64 * bin2byt64(struct BIN * bin, char * fromEndian, char * toEndian){
+// }
+// struct BYT64 * bin2byt64(struct BIN * bin, char * fromEndian, char * toEndian){
 
-}
+// }
 
-//BYT8 CONVERSIONS
-struct DEC * byt82dec(struct BYT8 * byt8, char * fromEndian){
+// //BYT8 CONVERSIONS
+// struct DEC * byt82dec(struct BYT8 * byt8, char * fromEndian){
 
-}
-struct HEX * byt82hex(struct BYT8 * byt8, char * fromEndian, char * toEndian){
+// }
+// struct HEX * byt82hex(struct BYT8 * byt8, char * fromEndian, char * toEndian){
 
-}
-struct BIN * byt82bin(struct BYT8 * byt8, char * fromEndian, char * toEndian){
+// }
+// struct BIN * byt82bin(struct BYT8 * byt8, char * fromEndian, char * toEndian){
 
-}
-struct B64 * byt82b64(struct BYT8 * byt8, char * fromEndian, char * toEndian){
+// }
+// struct B64 * byt82b64(struct BYT8 * byt8, char * fromEndian, char * toEndian){
 
-}
-struct BYT16 * byt82byt16(struct BYT8 * byt8, char * fromEndian, char * toEndian){
+// }
+// struct BYT16 * byt82byt16(struct BYT8 * byt8, char * fromEndian, char * toEndian){
 
-}
-struct BYT32 * byt82byt32(struct BYT8 * byt8, char * fromEndian, char * toEndian){
+// }
+// struct BYT32 * byt82byt32(struct BYT8 * byt8, char * fromEndian, char * toEndian){
 
-}
-struct BYT64 * byt82byt64(struct BYT8 * byt8, char * fromEndian, char * toEndian){
+// }
+// struct BYT64 * byt82byt64(struct BYT8 * byt8, char * fromEndian, char * toEndian){
 
-}
+// }
 
-//BYT16 CONVERSIONS
-struct DEC * byt162dec(struct BYT16 * byt16, char * fromEndian){
+// //BYT16 CONVERSIONS
+// struct DEC * byt162dec(struct BYT16 * byt16, char * fromEndian){
 
-}
-struct HEX * byt162hex(struct BYT16 * byt16, char * fromEndian, char * toEndian){
+// }
+// struct HEX * byt162hex(struct BYT16 * byt16, char * fromEndian, char * toEndian){
 
-}
-struct BIN * byt162bin(struct BYT16 * byt16, char * fromEndian, char * toEndian){
+// }
+// struct BIN * byt162bin(struct BYT16 * byt16, char * fromEndian, char * toEndian){
 
-}
-struct B64 * byt162b64(struct BYT16 * byt16, char * fromEndian, char * toEndian){
+// }
+// struct B64 * byt162b64(struct BYT16 * byt16, char * fromEndian, char * toEndian){
 
-}
-struct BYT8 * byt162byt8(struct BYT16 * byt16, char * fromEndian, char * toEndian){
+// }
+// struct BYT8 * byt162byt8(struct BYT16 * byt16, char * fromEndian, char * toEndian){
 
-}
-struct BYT32 * byt162byt32(struct BYT16 * byt16, char * fromEndian, char * toEndian){
+// }
+// struct BYT32 * byt162byt32(struct BYT16 * byt16, char * fromEndian, char * toEndian){
 
-}
-struct BYT64 * byt162byt64(struct BYT16 * byt16, char * fromEndian, char * toEndian){
+// }
+// struct BYT64 * byt162byt64(struct BYT16 * byt16, char * fromEndian, char * toEndian){
 
-}
+// }
 
-//BYT32 CONVERSIONS
-struct DEC * byt322dec(struct BYT32 * byt32, char * fromEndian){
+// //BYT32 CONVERSIONS
+// struct DEC * byt322dec(struct BYT32 * byt32, char * fromEndian){
 
-}
-struct HEX * byt322hex(struct BYT32 * byt32, char * fromEndian, char * toEndian){
+// }
+// struct HEX * byt322hex(struct BYT32 * byt32, char * fromEndian, char * toEndian){
 
-}
-struct BIN * byt322bin(struct BYT32 * byt32, char * fromEndian, char * toEndian){
+// }
+// struct BIN * byt322bin(struct BYT32 * byt32, char * fromEndian, char * toEndian){
 
-}
-struct B64 * byt322b64(struct BYT32 * byt32, char * fromEndian, char * toEndian){
+// }
+// struct B64 * byt322b64(struct BYT32 * byt32, char * fromEndian, char * toEndian){
 
-}
-struct BYT8 * byt322byt8(struct BYT32 * byt32, char * fromEndian, char * toEndian){
+// }
+// struct BYT8 * byt322byt8(struct BYT32 * byt32, char * fromEndian, char * toEndian){
 
-}
-struct BYT16 * byt322byt16(struct BYT32 * byt32, char * fromEndian, char * toEndian){
+// }
+// struct BYT16 * byt322byt16(struct BYT32 * byt32, char * fromEndian, char * toEndian){
 
-}
-struct BYT64 * byt322byt64(struct BYT32 * byt32, char * fromEndian, char * toEndian){
+// }
+// struct BYT64 * byt322byt64(struct BYT32 * byt32, char * fromEndian, char * toEndian){
 
-}
+// }
 
-//BYT64 CONVERSIONS
-struct DEC * byt642dec(struct BYT64 * byt64, char * fromEndian){
+// //BYT64 CONVERSIONS
+// struct DEC * byt642dec(struct BYT64 * byt64, char * fromEndian){
 
-}
-struct HEX * byt642hex(struct BYT64 * byt64, char * fromEndian, char * toEndian){
+// }
+// struct HEX * byt642hex(struct BYT64 * byt64, char * fromEndian, char * toEndian){
 
-}
-struct BIN * byt642bin(struct BYT64 * byt64, char * fromEndian, char * toEndian){
+// }
+// struct BIN * byt642bin(struct BYT64 * byt64, char * fromEndian, char * toEndian){
 
-}
-struct B64 * byt642b64(struct BYT64 * byt64, char * fromEndian, char * toEndian){
+// }
+// struct B64 * byt642b64(struct BYT64 * byt64, char * fromEndian, char * toEndian){
 
-}
-struct BYT8 * byt642byt8(struct BYT64 * byt64, char * fromEndian, char * toEndian){
+// }
+// struct BYT8 * byt642byt8(struct BYT64 * byt64, char * fromEndian, char * toEndian){
 
-}
-struct BYT16 * byt642byt16(struct BYT64 * byt64, char * fromEndian, char * toEndian){
+// }
+// struct BYT16 * byt642byt16(struct BYT64 * byt64, char * fromEndian, char * toEndian){
 
-}
-struct BYT32 * byt642byt32(struct BYT64 * byt64, char * fromEndian, char * toEndian){
+// }
+// struct BYT32 * byt642byt32(struct BYT64 * byt64, char * fromEndian, char * toEndian){
 
-}
+// }
--- C/Utils/Types/String/Test.c
+++ C/Utils/Types/String/Test.c
@@ -12,94 +12,94 @@
 
 
 
-int stringTest(){
-    // printf("String Test\n");
-    // struct String * str = genStr(10);
-    // return 0;
-}
+// int stringTest(){
+//     // printf("String Test\n");
+//     // struct String * str = genStr(10);
+//     // return 0;
+// }
 
 
-int addAtStringTest(){
-    return 0;
-}
+// int addAtStringTest(){
+//     return 0;
+// }
 
-int sliceStringTest(){
-    return 0;
-}
-
-
-int copyStringTest(){
-    return 0;
-}
-
-int removeAtStringTest(){
-    return 0;
-}
-
-int removeAllStringTest(){
-    return 0;
-}
-
-int trimStringTest(){
-    return 0;
-}
-
-int trimRightStringTest(){
-    return 0;
-}
-
-int trimLeftStringTest(){
-    return 0;
-}
-
-int reallocStringTest(){
-    return 0;
-}
-
-int mallocStringTest(){
-    return 0;
-}
-
-int callocStringTest(){
-    return 0;
-}
-
-int compareStringTest(){
-    return 0;
-}
-
-int concatStringTest(){
-    return 0;
-}
-
-int sizeStringTest(){
-    return 0;
-}
-
-int substrStringTest(){
-    return 0;
-}
+// int sliceStringTest(){
+//     return 0;
+// }
 
 
-int testString(){
-    //stringTest(1);
-    // sliceTest();
-    // copyTest();
-    // removeAtTest();
-    // addAtTest();
-    // removeAllTest();
-    // sizeTest();
-    // trimTest();
-    // trimRightTest();
-    // trimLeftTest();
-    // reallocTest();
-    // mallocTest();
-    // callocTest();
-    // nextTest();
-    // prevTest();
-    // concatTest();
-    // compareTest();
-    // sizeTest();
-    // substrTest();
-}
+// int copyStringTest(){
+//     return 0;
+// }
+
+// int removeAtStringTest(){
+//     return 0;
+// }
+
+// int removeAllStringTest(){
+//     return 0;
+// }
+
+// int trimStringTest(){
+//     return 0;
+// }
+
+// int trimRightStringTest(){
+//     return 0;
+// }
+
+// int trimLeftStringTest(){
+//     return 0;
+// }
+
+// int reallocStringTest(){
+//     return 0;
+// }
+
+// int mallocStringTest(){
+//     return 0;
+// }
+
+// int callocStringTest(){
+//     return 0;
+// }
+
+// int compareStringTest(){
+//     return 0;
+// }
+
+// int concatStringTest(){
+//     return 0;
+// }
+
+// int sizeStringTest(){
+//     return 0;
+// }
+
+// int substrStringTest(){
+//     return 0;
+// }
+
+
+// int testString(){
+//     //stringTest(1);
+//     // sliceTest();
+//     // copyTest();
+//     // removeAtTest();
+//     // addAtTest();
+//     // removeAllTest();
+//     // sizeTest();
+//     // trimTest();
+//     // trimRightTest();
+//     // trimLeftTest();
+//     // reallocTest();
+//     // mallocTest();
+//     // callocTest();
+//     // nextTest();
+//     // prevTest();
+//     // concatTest();
+//     // compareTest();
+//     // sizeTest();
+//     // substrTest();
+// }
 
