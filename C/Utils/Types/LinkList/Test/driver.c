c101ee09c0568375856a64ef1e7c6eb41c9a7f23
Zeus <zeusofolympus1000@gmail.com>
2022-12-21 19:56:17 -0500

    utilsTypesLinkListDevRun

---
+++ C/Utils/Types/LinkList/Test/driver.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "./driver.h"
+int main(){
+    printf("Utils/Utils/Types/LinkList/Test/Driver\n");
+
+    return 0;
+}
---
+++ C/Utils/Types/LinkList/Test/driver.h
@@ -0,0 +1,5 @@
+#ifndef UTILS_TYPES_LINKLIST_TEST_DRIVER
+#define UTILS_TYPES_LINKLIST_TEST_DRIVER
+#include "./Test.h"
+
+#endif
