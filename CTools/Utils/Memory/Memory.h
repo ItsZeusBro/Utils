5f3619848abec7fb8d26d2e0dc7a03810140bb55
Zeus <zeusofolympus1000@gmail.com>
2022-12-25 15:32:55 -0500

    garbage can definition

--- CTools/Utils/Memory/Memory.h
+++ CTools/Utils/Memory/Memory.h
@@ -1,6 +1,9 @@
 #ifndef CTOOLS_UTILS_MEMORY_MEMORY
 #define CTOOLS_UTILS_MEMORY_MEMORY
 
+struct Heap{
+    int sizeInBytes;    //this doesnt gaurantee that we wont have over allocations because heapSize is not always contiguous
+};
 void * safeMalloc();
 void * safeAlloc();
 void * safeReAlloc();
@@ -12,4 +15,6 @@ void * safeReAllocArray();
 void * safeMallocArray();
 void * safeCallocArray();
 void * safeMemAllign();
+void * heapSize();  //gets the available heap size
+int garbageCan(void * ptr, char * type); //Every type should have a function that frees its memory, and garbage can calls on these types
 #endif
