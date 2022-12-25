931d2cc710569306bba28471f32a6843fcb6d89e
Zeus <zeusofolympus1000@gmail.com>
2022-12-25 15:50:07 -0500

    saving recycling definition

--- CTools/Utils/Memory/Memory.h
+++ CTools/Utils/Memory/Memory.h
@@ -17,4 +17,10 @@ void * safeCallocArray();
 void * safeMemAllign();
 void * heapSize();  //gets the available heap size
 int garbageCan(void * ptr, char * type); //Every type should have a function that frees its memory, and garbage can calls on these types
+                                         //and there garbage functions to do so
+
+int recycle()                            //Long term expiriment: Every type should have a creation time and should be freed if it is not used for a long time
+                                         //by maintaining a recycling table. Depending on the type, we can analyze statistically how programs perform, and use
+                                         //this function to help our programs run longer before crashing due to memory leaks. This is good for massively parallel
+                                         //problems where there is less importance for any given process
 #endif
